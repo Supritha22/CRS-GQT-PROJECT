458d7c9889de038576457d379d71629a777720b0
Supritha22 <supritha200022@gmail.com>
2022-01-29 17:22:14 +0530

    the first push

---
+++ CRS-GQT-COLLECTION-PROJECT/src/com/gqt/crs/entity/courses.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.gqt.crs.entity;
+
+/**
+ * @author Supritha
+ *
+ */
+public class courses {
+
+}
---
+++ CRS-GQT-COLLECTION-PROJECT/src/com/gqt/crs/entity/student.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.gqt.crs.entity;
+
+/**
+ * @author {supritha}
+ *
+ 
+ */
+public class student {
+
+}
